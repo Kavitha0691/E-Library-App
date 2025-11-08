79ac52875f0893b9e47c1605beee063a2c693580
kavitha0691 <kavitha0691@gmail.com>
2025-11-08 18:09:15 +0100

    added changes

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://lgjzetyzbxuxzjcmrrij.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxnanpldHl6Ynh1eHpqY21ycmlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0NTI2MTcsImV4cCI6MjA3ODAyODYxN30.0SXVnaGi0kGeKmC4HObF3pv1HsK6CUFhyeN1q2oBtA0';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
